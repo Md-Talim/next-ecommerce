a5c808b2a85be5080dc34d021546bdde967df221
Mohammad Talim <talimmohammad116@gmail.com>
2023-09-12 20:59:08 +0530

    Add stripe customer id to database Add the stripe customer id when creating user.

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,8 @@
-import NextAuth from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { PrismaClient } from '@prisma/client';
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import Stripe from 'stripe';
 
 const prisma = new PrismaClient();
 
@@ -13,4 +14,23 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  events: {
+    createUser: async ({ user }) => {
+      const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+        apiVersion: '2023-08-16',
+      });
+
+      // create a stripe customer
+      const costomer = await stripe.customers.create({
+        name: user.name || undefined,
+        email: user.email || undefined,
+      });
+
+      // update the prisma user with the stripe customer id
+      await prisma.user.update({
+        where: { id: user.id },
+        data: { stripeCustomerId: costomer.id },
+      });
+    },
+  },
 });
